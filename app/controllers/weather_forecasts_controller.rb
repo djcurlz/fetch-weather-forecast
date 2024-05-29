36fa0604ca61506fb5311c8e945bd9be89e15520
Jim Curlee <jamescurlee@gmail.com>
2024-05-28 21:59:58 -0600

    chore: remove debug code

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -5,22 +5,17 @@ class WeatherForecastsController < ApplicationController
     if params[:address]
       begin
         @address = params[:address]
-        puts "DEBUG: address = #{@address}"
+        # Send address to Geocoder to fetch location values
         response = Geocoder.search(@address)
-        puts "DEBUG: response = #{response}"
         if response.is_a?(Array) && response.first.is_a?(Geocoder::Result::Base)
           # Process the response
           latitude = response[0].data['properties']['lat'] 
           longitude = response[0].data['properties']['lon'] 
           country_code = response[0].data["properties"]["country_code"]
           postal_code = response[0].data["properties"]["postcode"]
-          puts "DEBUG: latitude = #{latitude}"
-          puts "DEBUG: longitude = #{longitude}"
-          puts "DEBUG: country_code = #{country_code}"
-          puts "DEBUG: postal_code = #{postal_code}"
           @weather_cache_key = "#{country_code}/#{postal_code}"
           @weather_cache_pulled = Rails.cache.exist?(@weather_cache_key)
-          puts "DEBUG: weather_cache_pulled = #{@weather_cache_pulled}"
+          # Cache forecast details for 30 minutes
           @weather = Rails.cache.fetch(@weather_cache_key, expires_in: 30.minutes) do
             WeatherService.call(latitude, longitude)          
           end
