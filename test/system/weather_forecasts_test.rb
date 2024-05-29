4c566e56b175cc66b6a3568846ee58d7ca3b8689
Jim Curlee <jamescurlee@gmail.com>
2024-05-28 21:47:14 -0600

    feat: controller and system tests

---
+++ test/system/weather_forecasts_test.rb
@@ -0,0 +1,17 @@
+require "application_system_test_case"
+
+class WeatherForecastsTest < ApplicationSystemTestCase
+  require "application_system_test_case"
+
+  test "show" do
+    address = Faker::Address.full_address
+    visit url_for \
+      controller: "weather_forecasts", 
+      action: "show", 
+      params: { 
+        address: address 
+      }
+    assert_selector "h1", text: "Weather Forecast Details"
+  end
+
+end
